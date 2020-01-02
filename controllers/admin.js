0b2f846905a99a9452d00fd83bce7d6ceb61e379
JerryG09 <phemmy09@gmail.com>
2020-01-02 08:56:18 +0100

    admin controller to add products

---
+++ controllers/admin.js
@@ -0,0 +1,12 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
