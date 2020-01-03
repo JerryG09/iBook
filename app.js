f0c73ed4a9c5ac3d29d4254557b483a090b81062
JerryG09 <phemmy09@gmail.com>
2020-01-03 09:32:48 +0100

    implemented multer for image upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -21,6 +22,26 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+// Configure multer to rename file before storing
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname)
+  }
+})
+
+// Configure file filter 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  }
+  else {
+    cb(null, false)
+  }
+}
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -29,6 +50,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
